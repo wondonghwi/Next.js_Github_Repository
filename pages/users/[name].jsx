bded54f9840d1a21a0d65c509c58996d5c37a241
원동휘 <donghwi1289@gmail.com>
2021-06-11 22:58:11 +0900

    ADD : Repositories컴포넌트 추가

--- pages/users/[name].jsx
+++ pages/users/[name].jsx
@@ -1,28 +1,54 @@
 import fetch from 'isomorphic-unfetch';
+import css from 'styled-jsx/css';
 import Profile from '../../components/Profile';
+import Repositories from '../../components/Repositories';
 
-const name = ({ user }) => {
-  if (!user) {
-    return null;
+const style = css`
+  .user-contents-wrapper {
+    padding: 20px;
+    display: flex;
   }
+`;
+
+const name = ({ user, repos }) => {
   return (
-    <>
+    <div className="user-contents-wrapper">
       <Profile user={user} />
-    </>
+      <Repositories user={user} repos={repos} />
+      <style jsx>{style}</style>
+    </div>
   );
 };
 
 export const getServerSideProps = async ({ query }) => {
-  const { name } = query;
+  const { name, page = '1' } = query;
   try {
-    const res = await fetch(`https://api.github.com/users/${name}`);
-    if (res.status === 200) {
-      const user = await res.json();
-      return { props: { user } };
+    let user;
+    let repos;
+
+    const userRes = await fetch(`https://api.github.com/users/${name}`, {
+      headers: {
+        Authorization: 'token 714e48da28db7533dd9dafe6f4f040b142d30560',
+      },
+    });
+    if (userRes.status === 200) {
+      user = await userRes.json();
+    } else {
+      throw Error(userRes.statusText);
     }
-    return { props: {} };
+    const repoRes = await fetch(`https://api.github.com/users/${name}/repos?sort=updated&page=${page}&per_page=10`, {
+      headers: {
+        Authorization: 'token 714e48da28db7533dd9dafe6f4f040b142d30560',
+      },
+    });
+    if (repoRes.status === 200) {
+      repos = await repoRes.json();
+    } else {
+      throw Error(userRes.statusText);
+    }
+    return { props: { user, repos } };
   } catch (e) {
-    console.log(e);
+    console.log(e.message);
     return { props: {} };
   }
 };
